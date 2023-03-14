9e0b4c02a305b030d9ea87336ec37590d7b9f8aa
Arttu Mutka <Arttu.mutka@gmail.com>
2023-03-14 16:26:50 +0200

    Made bunch of visual changes. Also added json upda

---
+++ TVRaiser Monitor/js/Updates.js
@@ -0,0 +1,55 @@
+//Json fetch functions
+//Weather
+function weather() {
+        //Updates the weather
+        (async () => {
+                const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=60.81&longitude=23.62&current_weather=true&timezone=auto`);
+                const json = await res.json();
+                        document.getElementById("Latitude").innerHTML = "latitude: "+json.latitude;
+                        document.getElementById("Longitude").innerHTML = "longitude: "+json.longitude;
+                        document.getElementById("Temperature").innerHTML = json.current_weather.temperature+"°C";
+              })();
+        }
+//Coffee
+function Coffee() {
+        //Updates the coffee status
+        (async () => {
+                const res = await fetch(`https://bezainternational.org/tvr/`);
+                const json = await res.json();
+                        if (json.disable == 0) {
+                        document.getElementById("CoffeeStatus").innerHTML = "Coffee status: "+json.title;
+                        document.getElementById("CoffeeDescription").innerHTML = json.content;
+                        }
+                        else if (json.disable == 1) {
+                        document.getElementById("CoffeeStatus").innerHTML = "Coffee disabled";
+                        document.getElementById("CoffeeDescription").innerHTML = "Coffee is disabled";
+                        }     
+              })();
+        }
+
+//Update functions
+
+//Updates every second
+setInterval(function() {
+      
+      //Date functions
+      var today = new Date();
+      var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      //Time functions
+      document.getElementById("TimeText").innerHTML = "Time: "+time;
+      document.getElementById("DateText").innerHTML = "Date: "+date;
+              }, 1000);
+
+//5 minute updates
+setInterval(function() {
+        //Updates the weather every 5 minutes
+        weather();
+        //Updates the coffee status every 5 minutes
+        Coffee();
+              }, 300000);
+
+
+//On load
+weather();
+Coffee();
--- TVRaiser Monitor/js/functions.js
+++ TVRaiser Monitor/js/functions.js
@@ -8,29 +8,3 @@ function SideB_Open() {
     document.getElementById("Sidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
   }
-
-  //Json functions
-  /*fetch('https://bezainternational.org/tvr/public/')
-          .then(res => res.json())
-          .then((out) => {
-              console.log('Output: ', out);
-      }).catch(err => console.error(err));*/
-
-  fetch('https://api.open-meteo.com/v1/forecast?latitude=60.81&longitude=23.62&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin')
-          .then(res => res.json())
-          .then((out) => {
-              console.log('Output: ', out);
-              document.getElementById("Latitude").innerHTML = "latitude: "+out.latitude;
-              document.getElementById("Longitude").innerHTML = "longitude: "+out.longitude;
-              document.getElementById("Temperature").innerHTML = out.hourly.temperature_2m[0]+"°C";
-      }).catch(err => console.error(err));
-
-      
-
-    //Date functions
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    //Time functions
-    document.getElementById("TimeText").innerHTML = "Time: "+time;
-    document.getElementById("DateText").innerHTML = "Date: "+date;
