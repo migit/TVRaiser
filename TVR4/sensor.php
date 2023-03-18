99d052d2dde80ad8b3dc01346eb76195387487b3
Michael Seyoum <michael13moges@gmail.com>
2023-03-18 12:57:55 +0200

    Update sensor.php

--- TVR4/sensor.php
+++ TVR4/sensor.php
@@ -1,17 +1,15 @@
 <?php
 $con = mysqli_connect('host_name', 'database_user', 'database_password','database_name');
 
-$txtTitle = $_POST['txtTitle'];
-$txtContent = $_POST['txtContent'];
+$txtTitle = "Coffee is ready!";
+$txtContent = "Coffee has been brewed at temprature: "; //+ $_POST["temp"]; 
 $sql = "INSERT INTO `notification` (`id`, `title`, `content`) VALUES ('', '$txtTitle', '$txtContent')";
-
 $rs = mysqli_query($con, $sql);
-
 if($rs)
 {
 	echo "sensor data Inserted";
 }
+mysqli_close($con);
 
-echo $rs;
 
 ?>
