ea60b653167823495fa7a908e4a4738fb08de709
Arttu Mutka <Arttu.mutka@gmail.com>
2023-03-20 16:52:49 +0200

    Made some modularity

--- TVRaiser Monitor/js/Create.js
+++ TVRaiser Monitor/js/Create.js
@@ -2,7 +2,7 @@
 var i = 0;
 
 //Function to create cards
-function CreateCard(CardHeader, CardContent = [], CardSize, CardIcon) {
+function CreateCard(CardHeader, CardContent = [], CardSize, CardIcon, CardAnimation) {
     // handle empty variables
     if (CardHeader == null) {
         CardHeader = "Card Header";
@@ -67,9 +67,33 @@ function CreateCard(CardHeader, CardContent = [], CardSize, CardIcon) {
             CardIcon = "fa fa-calendar";
             break;
     }
+    //Convert CardAnimation variables to right format
+    switch(CardAnimation) {
+        case "None":
+            CardAnimation = "";
+            break;
+        case "Pulse":
+            CardAnimation = "fa-pulse ";
+            break;
+        case "Fade":
+            CardAnimation = "fa-fade";
+            break;
+        case "Beat":
+            CardAnimation = "fa-beat";
+            break;
+        case "Bounce":
+            CardAnimation = "fa-bounce";
+            break;
+        case "Spin":
+            CardAnimation = "fa-spin";
+            break;
+        case "Shake":
+            CardAnimation = "fa-shake";
+            break;
+    }
     //Used to create card div
     let IdText = CardHeader.replace(/ /g, "_");
-    document.getElementById('CardHolder').innerHTML += '<div id="'+IdText+"_Card"+'" class="w3-card-4 w3-round-large w3-margin '+CardSize+' "> <header id="'+IdText+"_Card_header"+'" class="w3-container w3-theme w3-round-large"> <h1 id="'+IdText+"_Card_title"+'">'+CardHeader+'</h1> <p id="'+IdText+"_Card_icon"+'" class="'+CardIcon+' w3-margin w3-jumbo"></p> </header> <div id="'+IdText+"_Card_container"+'" class="w3-container"> </div> </div> </div>';
+    document.getElementById('CardHolder').innerHTML += '<div id="'+IdText+"_Card"+'" class="w3-card-4 w3-round-large w3-margin '+CardSize+' "> <header id="'+IdText+"_Card_header"+'" class="w3-container w3-theme w3-round-large"> <h1 id="'+IdText+"_Card_title"+'">'+CardHeader+'</h1> <p style = "margin-left: 80px;" id="'+IdText+"_Card_icon"+'" class="'+CardIcon+" "+CardAnimation+' w3-margin w3-jumbo"></p> </header> <div id="'+IdText+"_Card_container"+'" class="w3-container"> </div> </div> </div>';
     //Used to create card content
     CardContent.forEach(element => {
         document.getElementById(IdText+'_Card_container').innerHTML += "<p id="+IdText+"_Card_content_"+i+">"+CardContent[i]+"</p>";
@@ -79,7 +103,3 @@ function CreateCard(CardHeader, CardContent = [], CardSize, CardIcon) {
 }
 
 //Usage of CreateCard function first parameter is the card header, second parameter is the card content, third parameter is the card size, fourth parameter is the card icon
-CreateCard("Coffee status", ["Coffee status: ", "Description: ", "Level: <i>Full</i>", "Temperature: <i>160<sup>o</sup>C</i>"], "Medium", "CoffeeMug");
-CreateCard("Weather", ["Weather status: ", "Current temperature: ", "Longitude: ", "Latitude: "], "Small", "Sun");
-CreateCard("Time", ["Current time: ", "Current date: "], "Medium", "Clock");
-CreateCard("Notifications", ["Notifications: "], "Medium", "Bell");
